ab3b70e80b9e0af26415893cc443fe1b875d5831
Yehnan Chiang <yehnan174@gmail.com>
2013-10-18 11:34:10 +0800

    reorg

---
+++ sketchbook/ch03_led_switch/ch03_led_switch.ino
@@ -0,0 +1,13 @@
+#define LED_PIN 11
+#define SWITCH_PIN 7
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT);
+}
+
+void loop() {
+  boolean switch_status = digitalRead(SWITCH_PIN);
+  digitalWrite(LED_PIN, switch_status);
+}
+
