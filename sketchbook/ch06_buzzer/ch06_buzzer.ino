12e6626d60882d6e34718548f27f861bafa7daa9
Yehnan Chiang <yehnan174@gmail.com>
2013-10-24 16:04:21 +0800

    6.1 buzzer

---
+++ sketchbook/ch06_buzzer/ch06_buzzer.ino
@@ -0,0 +1,18 @@
+#include "pitches.h"
+
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_, NOTE_B3, NOTE_C4
+};
+
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+}
+
+void loop() {
+  play(melody, noteDurations, sizeof(melody) / sizeof(int));
+  delay(2000);
+}
+
---
+++ sketchbook/ch06_buzzer/pitches.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "pitches.h"
+
+void play(int *melody, int *noteDurations, int num){
+  for(int note = 0; note < num; note++){
+    int noteDuration = 3000 / noteDurations[note];
+    tone(BUZZER_PIN, melody[note], noteDuration);
+    delay(noteDuration * 1.30);
+  }
+}
+
+
