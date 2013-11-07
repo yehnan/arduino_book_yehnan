dfabc0931dfc28440934ac581ece53512d85891c
Yehnan Chiang <yehnan174@gmail.com>
2013-11-07 23:33:19 +0800

    servo

---
+++ sketchbook/ch10_servo/ch10_servo.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+#define SERVO_PIN 9
+Servo s; // 建立Servo物件
+void setup() {
+  s.attach(SERVO_PIN); // 連接伺服馬達（訊號線）
+}
+void loop() {
+  int i;
+  for(i = 0; i <= 180; i++){
+    s.write(i); // 使用write，傳入角度，從0度轉到180度
+    delay(20);
+  }
+  for(i = 180; i >= 0; i--){
+    s.write(i); // 從180度轉回到0度
+    delay(20);
+  }
+}
+
---
+++ sketchbook/ch10_servo_pulsewidth/ch10_servo_pulsewidth.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+#define SERVO_PIN 9
+// 定義脈衝寬度上下限，此處以Tower Pro SG90為例
+#define PL_LOWER_BOUND 500 // PL是Pulse Width脈衝寬度
+#define PL_UPPER_BOUND 2400
+Servo s; // 建立Servo物件
+void setup() {
+  // 自行指定脈衝寬度上下限  
+  s.attach(SERVO_PIN, PL_LOWER_BOUND, PL_UPPER_BOUND);
+}
+void loop() {
+  s.writeMicroseconds(PL_LOWER_BOUND); // 直接指定脈衝寬度
+  delay(1000);
+  s.write(0);
+  delay(1000);
+  s.writeMicroseconds((PL_LOWER_BOUND+PL_UPPER_BOUND)/2);
+  delay(1000);
+  s.write(90);
+  delay(1000);
+  s.writeMicroseconds(PL_UPPER_BOUND);
+  delay(1000);
+  s.write(180);
+  delay(1000);
+}
+
