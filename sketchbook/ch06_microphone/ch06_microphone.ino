57e14d16e6c7eb642dd503d2dee368b3232864a4
Yehnan Chiang <yehnan174@gmail.com>
2013-11-07 15:04:38 +0800

    microphone

---
+++ sketchbook/ch06_microphone/ch06_microphone.ino
@@ -0,0 +1,19 @@
+#define SERIAL_BAUDRATE 19200
+#define MIC_PIN A0 // 麥克風
+#define VOLUME_HIGH_LEVEL 300 // 超過此值便當做有高音量
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+}
+
+int count = 0; // 計算收到幾次的高音量
+void loop() {
+  int mic = analogRead(MIC_PIN); // 讀取麥克風輸出
+  if(mic >= VOLUME_HIGH_LEVEL){
+    count++;
+    Serial.print("mic is ");
+    Serial.print(mic);
+    Serial.print(", count is ");
+    Serial.println(count);
+  }
+}
+
---
+++ sketchbook/ch06_microphone_debounce/ch06_microphone_debounce.ino
@@ -0,0 +1,24 @@
+#define MIC_PIN A0 // 麥克風
+#define LED_PIN 13 // 燈光
+#define VOLUME_HIGH_LEVEL 300 // 超過此值便當做有高音量
+#define DEBOUNCE_DELAY 50 // 除彈跳的延遲時間
+unsigned long time_previous;
+boolean ledStatus = LOW;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, ledStatus);
+  time_previous = millis();
+}
+void loop() {
+  int mic = analogRead(MIC_PIN); // 讀取麥克風輸出
+  if(mic >= VOLUME_HIGH_LEVEL){ // 若超過此音量
+    unsigned long time_current = millis();
+    if(time_current - time_previous > DEBOUNCE_DELAY){ // 已逾時
+      time_previous = time_current;
+      ledStatus = !ledStatus; // 切換LED明滅
+      digitalWrite(LED_PIN, ledStatus);
+    }
+  }
+}
+
