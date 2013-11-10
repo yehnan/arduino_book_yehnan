8cdb65ec93c8d725e397528ea41e2238c9f67069
Yehnan Chiang <yehnan174@gmail.com>
2013-11-10 15:27:56 +0800

    keypad

---
+++ sketchbook/ch04_3x4keyboard/ch04_3x4keyboard.ino
@@ -0,0 +1,43 @@
+#define SERIAL_BAUDRATE 19200
+// 定義矩陣鍵盤的針腳（A~G）與Arduino腳位的連接關係
+#define A_PIN 8
+#define B_PIN 7
+#define C_PIN 6
+#define D_PIN 5
+#define E_PIN 4
+#define F_PIN 3
+#define G_PIN 2
+#define COL 3 // 3直欄
+#define ROW 4 // 4橫列
+
+typedef struct{
+  int arduinoPin;
+  char keypadPin;
+}PinMapping;
+PinMapping pins[COL+ROW] = {
+  {A_PIN, 'A'}, {B_PIN, 'B'}, {C_PIN, 'C'}, {D_PIN, 'D'},
+  {E_PIN, 'E'}, {F_PIN, 'F'}, {G_PIN, 'G'},
+};
+
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+}
+void loop() {
+  for(int i = 0; i < COL+ROW; i++){ // 逐一測試每個針腳
+    pinMode(pins[i].arduinoPin, OUTPUT);
+    digitalWrite(pins[i].arduinoPin, LOW); // 設為低電位
+    for(int j = 0; j < COL+ROW; j++){
+      if(pins[j].arduinoPin != pins[i].arduinoPin){
+        pinMode(pins[j].arduinoPin, INPUT_PULLUP); // 啟用內建的上拉電阻
+        if(digitalRead(pins[j].arduinoPin) == LOW){ // 若某鍵被按下，其狀態會是LOW
+          Serial.print(pins[i].keypadPin); // 輸出是哪兩個針腳的交叉處
+          Serial.print(" and ");
+          Serial.println(pins[j].keypadPin);
+        }
+      }
+    }
+    digitalWrite(pins[i].arduinoPin, HIGH);
+    pinMode(pins[i].arduinoPin, INPUT);
+  }
+}
+
---
+++ sketchbook/ch04_3x4keyboard_library/ch04_3x4keyboard_library.ino
@@ -0,0 +1,42 @@
+#include <Keypad.h>
+#define SERIAL_BAUDRATE 19200
+// 定義矩陣鍵盤的針腳（A~G）與Arduino腳位的連接關係
+#define A_PIN 8
+#define B_PIN 7
+#define C_PIN 6
+#define D_PIN 5
+#define E_PIN 4
+#define F_PIN 3
+#define G_PIN 2
+#define COL 3 // 3直欄
+#define ROW 4 // 4橫列
+
+// 定義矩陣鍵盤的針腳（A~G）與列欄的關係
+#define COL_0_PIN A_PIN
+#define COL_1_PIN B_PIN
+#define COL_2_PIN C_PIN
+#define ROW_0_PIN D_PIN
+#define ROW_1_PIN E_PIN
+#define ROW_2_PIN F_PIN
+#define ROW_3_PIN G_PIN
+byte rowPins[ROW] = {ROW_0_PIN, ROW_1_PIN, ROW_2_PIN, ROW_3_PIN};
+byte colPins[COL] = {COL_0_PIN, COL_1_PIN, COL_2_PIN};
+char keys[ROW][COL] = { // 每個按鍵所對應的字元
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+// 建立Keypad物件
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROW, COL);
+
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+}
+void loop() {
+  char key = keypad.getKey();
+  if(key != NO_KEY){
+    Serial.println(key);
+  }
+}
+
