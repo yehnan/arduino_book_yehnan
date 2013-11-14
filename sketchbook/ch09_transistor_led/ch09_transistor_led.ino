8f1d921f8541e336434e3d304b76812308af9002
Yehnan Chiang <yehnan174@gmail.com>
2013-11-14 23:35:45 +0800

    transistor

---
+++ sketchbook/ch09_transistor_led/ch09_transistor_led.ino
@@ -0,0 +1,13 @@
+#define LED_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
+
