61d1b745182775b1a4db3bda44af3a4d59fb6f11
Yehnan Chiang <yehnan174@gmail.com>
2013-11-18 23:21:05 +0800

    backup

--- sketchbook/ch09_darlington_array/ch09_darlington_array.ino
+++ sketchbook/ch09_darlington_array/ch09_darlington_array.ino
@@ -1,24 +1,25 @@
-#define LED_NUM 1
-#define BUZZER_PIN 3
-int ledPins[LED_NUM] = {
-  2, //3, 4, 
-};
+#define LED_PIN 2 // LED
+#define BUZZER_PIN 3 // 蜂鳴器
+#define FAN_PIN 4 // 風扇
+#define BULB_PIN 5 // 燈泡
 #define NOTE_C5  523
-void setup() {
-  for(int i = 0; i < LED_NUM; i++){
-    pinMode(ledPins[i], OUTPUT);
-  }
-}
+#define NOTE_B5  988
 
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+  pinMode(BULB_PIN, OUTPUT);
+}
 void loop() {
-  tone(BUZZER_PIN, NOTE_C5, 3000/8);
-  for(int i = 0; i < LED_NUM; i++){
-    digitalWrite(ledPins[i], HIGH);
-  }
-  delay(1000);
-  for(int i = 0; i < LED_NUM; i++){
-    digitalWrite(ledPins[i], LOW);
-  }
-  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
+  tone(BUZZER_PIN, NOTE_C5, 3000 / 4);
+  digitalWrite(FAN_PIN, HIGH);
+  digitalWrite(BULB_PIN, HIGH);
+  delay(3000);
+  digitalWrite(LED_PIN, LOW);
+  tone(BUZZER_PIN, NOTE_B5, 3000 / 4);
+  digitalWrite(FAN_PIN, LOW);
+  digitalWrite(BULB_PIN, LOW);
+  delay(3000);
 }
 
