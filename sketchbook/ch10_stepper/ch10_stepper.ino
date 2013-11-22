67667a69947e465af05a06d0c5fdb23fa8620a4a
Yehnan Chiang <yehnan174@gmail.com>
2013-11-22 23:07:13 +0800

    stepper

---
+++ sketchbook/ch10_stepper/ch10_stepper.ino
@@ -0,0 +1,28 @@
+#include <Stepper.h>
+#define SERIAL_BAUDRATE 19200
+// 連接步進馬達的腳位
+#define IN1_PIN 12
+#define IN2_PIN 11
+#define IN3_PIN 10
+#define IN4_PIN 9
+#define STEPS 512 // 此步進馬達轉一圈需幾步
+// 建立Stepper物件
+Stepper motor(STEPS, IN1_PIN, IN2_PIN, IN3_PIN, IN4_PIN);
+
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
+  
+  motor.setSpeed(30); // 設定轉動速度，單位是RPM
+}
+void loop() {
+  if(Serial.available()) {
+    int steps = Serial.parseInt(); // 將ASCII碼轉成int
+    motor.step(steps); // 叫步進馬達旋轉該步數
+    Serial.println(steps);
+  }
+}
+
---
+++ sketchbook/ch10_stepper_AccelStepper/ch10_stepper_AccelStepper.ino
@@ -0,0 +1,24 @@
+#include <AccelStepper.h>
+#define SERIAL_BAUDRATE 19200
+// 連接步進馬達的腳位
+#define IN1_PIN 12
+#define IN2_PIN 11
+#define IN3_PIN 10
+#define IN4_PIN 9
+
+AccelStepper stepper(AccelStepper::FULL4WIRE, IN1_PIN, IN2_PIN, IN3_PIN, IN4_PIN);
+
+void setup() {
+  Serial.begin(SERIAL_BAUDRATE);
+  stepper.setMaxSpeed(80 * 512 / 60);
+  stepper.setAcceleration(100);
+}
+void loop() {
+  stepper.run();
+  if(Serial.available()) {
+    int s = Serial.parseInt(); // 將ASCII碼轉成int
+    stepper.move(s);
+    Serial.println(s);
+  }
+}
+
